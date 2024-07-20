eb2b1fc3c094627412146aece81bafb8610434b9
kimyezzang97 <kimyezzang97@gmail.com>
2024-07-20 20:48:11 +0900

    CRUD API 해보기
    - HTTP Method를 활용한 CRUD
    - exception 날려보기
    - query & param 사용 해보기

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 /**
@@ -9,7 +9,8 @@ import { PostsService } from './posts.service';
  * commentCount: number;
  */
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -17,18 +18,130 @@ interface Post {
   commentCount: number;
 }
 
+let posts : PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_offical',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 999999
+  },
+  {
+    id: 2,
+    author: 'newjeans_offical',
+    title: '뉴진스 해린',
+    content: '노래 연습 하고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 999999
+  },
+  {
+    id: 3,
+    author: 'blackpink_offical',
+    title: '블랙핑크 로제',
+    content: '공연하고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 999999
+  }
+]
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @Get('')
-  getPost(): Post { // Post 인터페이스 타입을 반환한다.
-    return {
-      author: 'newJeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 1000000,
-      commentCount: 99999,
+  // 1) GET /posts
+  // 모든 posts를 다 가져온다.
+  @Get()
+  getPosts(){
+    return posts;
+  }
+
+  // 2) GET /posts/:id
+  //  id에 해당되는 post를 가져온다.
+  //  예를 들어서 id=1일 경우 id가 1인 포스트를 가져온다.
+  @Get(':id')
+  getPost(@Param('id') id: string){
+    const post = posts.find((post) => post.id === +id);
+
+    if(!post){ // undefined이거나 null이거나 존재하지 않으면
+      throw new NotFoundException();
     }
+
+    return post;
+  }
+
+  // 3) POST /posts
+  //    POST를 생성한다.
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('comment') content: string,
+  ){
+   const post = {
+    id: posts[posts.length - 1].id + 1,
+    author,
+    title,
+    content,
+    likeCount: 0,
+    commentCount:0
+   };
+   posts = [
+    ...posts,
+    post
+   ];
+
+   return post;
+  }
+
+  // 4) PUT /posts/:id
+  //  id에 해당되는 POST를 변경한다.
+  @Put(":id")
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ){
+    const post = posts.find(post => post.id === +id);
+
+    if(!post){
+      throw new NotFoundException();
+    }
+
+    if(author) {
+      post.author = author;
+    }
+
+    if(title) {
+      post.title = title;
+    }
+
+    if(content) {
+      post.content = content;
+    }
+
+    posts = posts.map(prevPost => prevPost.id === +id ? post : prevPost);
+
+    return post;
+  }
+
+
+
+  // 5) DELETE /posts/:id
+  //  id에 해당되는 POST를 삭제한다.
+  @Delete(':id')
+  deletePost(
+    @Param('id') id: string,
+  ){
+    const post = posts.find(post => post.id !== +id);
+    
+    if(!post){
+      throw new NotFoundException();
+    }
+
+    posts = posts.filter(post => post.id !== +id);
+
+    return id;
   }
 }
